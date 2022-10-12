381b5deb1a2e4a556f131da20f972c55ba353444
Popular234 <cainjose24@g.crookcountyschools.org>
2022-10-12 14:36:19 -0700

    modified javascript file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+// console.log("hello world");
 
 let animal = 'RAT!!';
 
@@ -10,6 +10,26 @@ function mathIntro(number1, number2){
     console.log(" I have two numbers " + number1 + " and " + number2 + ". lets do some math with these numbers ")
 }
 
+function addition(number1, number2){
+    let EqualToo = number1 + number2
+    console.log("when added " + number1 + " plus " + number2 + " is equal too " + EqualToo)
+}
+
+function subtraction(number1 , number2){
+    let EqualToo = number1 - number2
+    console.log("when subtracted " + number1 + " minus " + number2 + " is equal too " + EqualToo)
+}
+
+function multiplication(number1, number2){
+    let Equaltoo = number1 * number2
+    console.log("when multiplied " + number1 + " times " + number2 + " is equal too " + Equaltoo)
+}
+
+function division(number1, number2){
+    let Equaltoo = number1 / number2
+    console.log("when divided " + number1 + " divided by " + number2 + " is equal too " + Equaltoo)
+}
+
 console.log(animal);
 
 let sum = number1 + number2;
@@ -17,8 +37,13 @@ let diff = number1 - number2;
 let mult = number1 * number2;
 let div = number2 / number1;
 
-console.log(mathIntro);
+addition(3, 4)
+subtraction(15, 12)
+multiplication(2, 3)
+division(20, 4)
+// mathIntro(2, 5);
 console.log(sum);
 console.log(diff);
 console.log(mult);
 console.log(div);
+console.log(mult);
