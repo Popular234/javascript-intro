91d023cc956ecfa3c70e65d7bdde2e2e5d1ca120
Popular234 <cainjose24@g.crookcountyschools.org>
2022-10-12 09:07:28 -0700

    added index, script, and style.css

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+let animal = 'RAT!!';
+
+var number1 = 5;
+
+var number2 = 10;
+
+function mathIntro(number1, number2){
+    console.log(" I have two numbers " + number1 + " and " + number2 + ". lets do some math with these numbers ")
+}
+
+console.log(animal);
+
+let sum = number1 + number2;
+let diff = number1 - number2;
+let mult = number1 * number2;
+let div = number2 / number1;
+
+console.log(mathIntro);
+console.log(sum);
+console.log(diff);
+console.log(mult);
+console.log(div);
